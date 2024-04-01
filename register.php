3e40392e9790eadc1f113011d60803b56de3b803
Riz-Kyx <112696806+Riz-Kyx@users.noreply.github.com>
2024-04-01 09:23:12 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,14 @@
+<?php
+require 'koneksi.php';
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_sql = "INSERT INTO tabel_user (username, email, password) 
+            VALUES ('$username', '$email', '$password')";
+
+if (mysqli_query($conn, $query_sql)) {
+    header("Location: login.html");
+} else {
+    echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
